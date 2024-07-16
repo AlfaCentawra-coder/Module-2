4148d177e3bfa0024b19b350b9746e5d5dcaedbd
AlfaCentawra-coder <vovaluzin55@gmail.com>
2024-07-16 16:28:24 +0500

    Update homework_2_3.py

--- homework_2_3.py
+++ homework_2_3.py
@@ -1,10 +1,11 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-x = len(my_list)
-while x >= 0:
+x = 0
+while x <= len(my_list):
     if int(my_list[x]) > 0:
         print(my_list[x])
-        x -= 1
+        x += 1
     elif int(my_list[x]) == 0:
+        x += 1
         continue
     else:
         break
