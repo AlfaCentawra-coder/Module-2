f170424d10afacf37b132db72bc9427acc7fa007
AlfaCentawra-coder <vovaluzin55@gmail.com>
2024-07-19 15:33:14 +0500

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,24 +1,22 @@
-def result(x):
-    result = []
-    for i in range(1, (x + 1)):
+def dividers(x):
+    d = []
+    for i in range(2, x + 1 ):
         if x % i == 0:
-            result.append(i)
-    return (result)
+            d.append(i)
+    return d
 
-def parol(y):
-    parol = []
-    for j in range(len(y)):
-        if y[j] == 1:
-            continue
-        for k in range(1, y[j]):
-            str_ = ''
-            chislo_1 = y[j] - k
-            chislo_2 = y[j] - chislo_1
-            str_ = str(chislo_2) + str(chislo_1)
-            parol.append(str_)
-    return parol
+def password(divs):
+    psswrd = []
+    for i in range(1, max(divs)):
+        for d in divs:
+            if (i < d):
+                if not(str(d - i) + str(i) in psswrd):
+                    psswrd.append(str(i) + str(d - i))
+    return psswrd
 
 
 x = int(input("Введите число: "))
-y = result(x)
-print(parol(y))
+d = dividers(x)
+p = password(d)
+for i in range(len(p)):
+    print(p[i], end="")
