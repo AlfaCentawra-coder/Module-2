182c003d3120bb161fc0b6820ab59e748c70408b
AlfaCentawra-coder <vovaluzin55@gmail.com>
2024-07-19 14:48:12 +0500

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,23 +1,24 @@
-def list(x):
+def result(x):
     result = []
     for i in range(1, (x + 1)):
         if x % i == 0:
             result.append(i)
     return (result)
 
-def new_list(y):
-    str_ = ""
-    for j in range (len(y)):
+def parol(y):
+    parol = []
+    for j in range(len(y)):
         if y[j] == 1:
             continue
-        for k in range (1, y[j]+1):
+        for k in range(1, y[j]):
+            str_ = ''
             chislo_1 = y[j] - k
             chislo_2 = y[j] - chislo_1
-            str_ = str_ + str(chislo_1) + str(chislo_2)
-    return(str_)
+            str_ = str(chislo_2) + str(chislo_1)
+            parol.append(str_)
+    return parol
 
 
 x = int(input("Введите число: "))
-y = list(x)
-z = new_list(y)
-print(z)
+y = result(x)
+print(parol(y))
