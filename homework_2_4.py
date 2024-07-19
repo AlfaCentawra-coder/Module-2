9c72fc3509ce32783b1f33420f2147422b11b1c1
AlfaCentawra-coder <vovaluzin55@gmail.com>
2024-07-19 19:34:45 +0500

    Update homework_2_4.py

--- homework_2_4.py
+++ homework_2_4.py
@@ -1,11 +1,11 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 997, 993]
 primes = []
 not_primes = []
 for i in range(len(numbers)):
     if numbers[i] == 1:
         continue
     z = 0
-    for x in range(1, (len(numbers)+1)):
+    for x in range(1, (numbers[i]+1)):
         if x <= numbers[i]:
             if (numbers[i] % x) == 0:
                 z += 1
