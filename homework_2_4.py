4fb7407b2062abed5fae53e7219dd4b298c0733d
AlfaCentawra-coder <vovaluzin55@gmail.com>
2024-07-19 11:26:11 +0500

    Update homework_2_4.py

--- homework_2_4.py
+++ homework_2_4.py
@@ -1,6 +1,6 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-primes = ""
-not_primes = ""
+primes = []
+not_primes = []
 for i in range(len(numbers)):
     if numbers[i] == 1:
         continue
@@ -10,7 +10,7 @@ for i in range(len(numbers)):
             if (numbers[i] % x) == 0:
                 z += 1
     if z >= 3:
-        not_primes = not_primes + str(numbers[i])
+        not_primes.append(numbers[i])
     else:
-        primes = primes + str(numbers[i])
+        primes.append(numbers[i])
 print("Простые числа:", primes, "Непростые числа: ", not_primes)
